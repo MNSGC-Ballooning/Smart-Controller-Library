4683ee45e33f43650a48bdefa6fd59bbc59ac490
Vinh Nguyen <nguy2876@umn.edu>
2019-06-05 10:29:36 -0500

    git status

---
+++ SmartController.cpp
@@ -0,0 +1,112 @@
+#include "SmartController.h"
+#include <Arduino.h>
+
+
+
+
+
+//Constructor
+SmartController::SmartController(int SmartNumber, HardwareSerial &print){
+  
+//For setting up Xbee
+Xbee = &print;
+/*******Initializing variables********/
+newTempChar=true;
+CutTimer=0;
+DataTimer=0;
+SmartCount=SmartNumber; //Number of smart units attached
+
+
+
+/************************************/
+
+
+}
+
+
+
+
+
+
+/*************Response Function***************/
+String SmartController::Response(){
+
+if(Xbee->available()>0){
+    response=Xbee->read();
+
+for (int i=0;i<SmartCount;i++){
+
+   //Temp sending check
+   if (response==TempStart[i]){
+
+    DataTimer=millis();
+    DataMSG=String(CutCommands[i])+" Temp: ";
+    response='0';
+    return GetData(TempConfirms[i]);
+   }
+}
+for (int i=0;i<SmartCount;i++){
+   //Cut confirmation check
+    if (response==CutConfirms[i] && CutStatuses[i]==0){
+     
+     DataMSG=String(CutCommands[i])+" Cut";
+     CutStatuses[i]=1;
+     response='0';
+     
+     //MAKE BETTER, is just sending to stop cut confirm spam
+     Xbee->print(CutAcknowledged[i]);
+     Xbee->print(CutAcknowledged[i]);
+     Xbee->print(CutAcknowledged[i]);
+     Xbee->print(CutAcknowledged[i]);
+     
+     return DataMSG;
+   }
+}
+   }
+
+response='0';
+return ""; //If dont have to log, will return ""
+}
+
+/*************Request Temp Function***********/
+void SmartController::RequestTemp(int SmartUnit){
+Xbee->print(TempRequests[SmartUnit-1]);
+}
+
+/*********Get Data Function*************/
+String SmartController::GetData(char id) {  //in unlikely event that temp send takes more than 2 seconds, break
+
+while(true && millis()-DataTimer<=2000){
+
+    if(Xbee->available()>0){
+    Data=Xbee->read();
+    }
+    if (Data==id){
+        DataTimer=millis();
+        Data='/';
+        newTempChar=true;
+        return DataMSG;  //ADD GPS TIMESTAMP
+
+    }
+  if(Data=='/'){
+    newTempChar=true;
+    }
+  else if(newTempChar && Data!='/')
+  {
+    DataMSG=DataMSG+String(Data);
+    newTempChar=false;
+  }
+}
+return "Took too long"; //if longer than 2 seconds
+}
+
+/**********Cut Command*****************/
+void SmartController::Cut(int SmartUnit,bool Condition){ ///Call repeatedly until cut confirmed
+if (Condition && CutStatuses[SmartUnit-1]==0)
+{
+  if (millis()-CutTimer>200){
+  Xbee->print(CutCommands[SmartUnit-1]);
+  CutTimer=millis();
+}
+}
+}
---
+++ SmartController.h
@@ -0,0 +1,55 @@
+#ifndef SMARTCONTROLLER_H_INCLUDED
+#define SMARTCONTROLLER_H_INCLUDED
+
+#include <Arduino.h>
+
+
+class SmartController
+{
+public:
+/***********Variables********/
+    char response;
+    char Data;
+    String DataMSG;
+    unsigned long CutTimer;
+    unsigned long DataTimer;
+    bool newTempChar;
+    int CutStatuses[5]={0,0,0,0,0};
+    
+/***********Character Arrays************/
+    const char CutCommands[2]={'A','B'};  //SEND, cut command. Can add more
+    const char CutConfirms[2]={'Z','X'};  //RECIEVE, indicates confirmed cut
+    const char CutAcknowledged[2]={'a','b'}; //SEND, to stop cut confirm spam in smarts 
+    const char TempRequests[2]={'O','P'}; //SEND, to request temperatures
+    const char TempStart[2]={'T','C'};    //RECIEVE, indicates start of temp send
+    const char TempConfirms[2]={'Y','V'}; //RECIEVE, indicates end of temp send
+/***************************************/
+
+/********Functions********/
+    String Response(); //Call in loop
+    String GetData(char id);  //Call from Response to get data string
+    void RequestTemp(int SmartUnit); //Call in main
+    void Cut(int SmartUnit,bool Condition); //Call with Condition=true to cut
+    //constructor
+    SmartController(int SmartNumber, HardwareSerial &print);
+    //ONLY NEED TO USE Constructor, Response(), RequestTemp(), and Cut() in main
+private:
+    int SmartCount; //Number of attached smarts
+    HardwareSerial* Xbee; //Xbee serial
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // SMARTCONTROLLER_H_INCLUDED
+
